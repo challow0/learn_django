bc31047a768ac8804525312e41a30a637090ca40
Challow <zqm_0011@163.com>
2019-11-07 22:53:34 +0800

    learn templates

--- tryjango/src/products/models.py
+++ tryjango/src/products/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-	title 		= models.CharField(max_length=120)
-	description = models.TextField()
-	price 		= models.TextField()
-	summary		= models.TextField(default='this is cool')
+	title 		= models.CharField(max_length=120) # max_length = required
+	description = models.TextField(blank=True,null=True)
+	price 		= models.DecimalField(max_digits = 19,decimal_places=2) # search the document
+	summary		= models.TextField(blank=False, null=False)
+	featured	= models.BooleanField() # null=True, default=True
