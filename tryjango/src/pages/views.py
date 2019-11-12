63dac544a3c97705c94f5b6a0880f71a7fbe4796
Challow <zqm_0011@163.com>
2019-11-12 23:15:11 +0800

    templates

--- tryjango/src/pages/views.py
+++ tryjango/src/pages/views.py
@@ -13,7 +13,15 @@ def contact_view(request, *args, **kwargs):
 	return render(request, "contact.html",{})
 
 def about_view(request, *args, **kwargs):
-	return render(request, "about.html",{})
+	my_context = {
+		"title": "This is about us",
+		"this_is_true":True,
+		"my_number": 123,
+		"my_list": [123,4324,765,"Abc"],
+		"my_html": "<h1>Hello World</h1>"
+	}
+
+	return render(request, "about.html",my_context)
 
 def social_view(request, *args, **kwargs):
 	return HttpResponse("<h1>Contact Page</h1>")
